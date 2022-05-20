7949a95007726c8fb98a4f3aed494cbef06cf4b8
Vikasreddy02 <105192846+Vikasreddy02@users.noreply.github.com>
2022-05-20 18:04:53 +0530

    Create widecoversion.java

---
+++ Pratical/widecoversion.java
@@ -0,0 +1,23 @@
+package packagecorejava;
+
+public class widecoversion {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			int i=320;
+			
+			//Auto type conversion from int to long
+			long l=i;
+			
+			//Auto type conversion from long to int
+			float f=l;
+			
+			System.out.println("Int value:"+i);
+			System.out.println("long value:"+l);
+			System.out.println("float value:"+f);
+			
+		}
+
+	}
+
